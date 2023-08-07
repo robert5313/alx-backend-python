82c95df6c0f7eb1842208f54c0b64a5385c1149c
robert5313 <kiptoorobert@zetech.ac.ke>
2023-08-07 21:28:09 +0000

    tasks async/await

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module 0 tasks under async/await
+"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+asyncio is a library to write concurrent code using the async/await syntax.
