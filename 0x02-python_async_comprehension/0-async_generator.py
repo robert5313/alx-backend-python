c456767721df7dcef228892becaf635caacfaeee
robert5313 <kiptoorobert@zetech.ac.ke>
2023-08-08 19:09:58 +0000

    async tasks

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''module 0
+'''
+
+import asyncio
+import random
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
