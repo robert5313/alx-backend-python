8e3e3a5240f199e6c8c8745ad25c5be3a582d22c
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-09 22:44:55 +0300

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-'''module 0
+'''Module 0 challenge task.
 '''
 
 import asyncio
 import random
 
 async def async_generator():
+    '''Generate a random sequence of numbers.
+    '''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
