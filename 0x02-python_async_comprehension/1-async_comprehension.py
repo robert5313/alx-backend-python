5ccaf2501fc72d22ed824c679e4b228ab222d84a
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-09 22:57:55 +0300

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''The execution of random n(end) times.
+    '''Executes wait_random n times.
     '''
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
