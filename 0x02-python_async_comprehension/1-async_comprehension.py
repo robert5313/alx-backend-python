808a95e4b39924ab869e8cca0a18ac7eeead594f
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-12 00:02:19 +0300

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 '''Module task 1.
 '''
-import asyncio
 from typing import List
+from importlib import import_module as using
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+async_generator = using('0-async_generator').async_generator
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes wait_random n times.
+async def async_comprehension() -> List[float]:
+    '''Creates a list of 10 numbers the generator.
     '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    return [num async for num in async_generator()]
