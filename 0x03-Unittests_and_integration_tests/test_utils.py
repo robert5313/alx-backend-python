b18d3c1ea29f7c2dde5bc77fcb9f657df260aaf5
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-28 23:28:02 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,7 +36,7 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """ Test for the utils.get_json function to check result."""
+        """ Test for the utils.get_json function to check result. """
         config = {'return_value.json.return_value': test_payload}
         patcher = patch('requests.get', **config)
         mock = patcher.start()
@@ -46,14 +46,14 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """ test class to tes utils.memoize"""
+    """ test class to tes utils.memoize """
 
     def test_memoize(self):
         """ Tests the function which occurs when calling a_property,
         the correct result is returned but a_result method """
 
         class TestClass:
-            """ Test Classin which a memoize wrapping is done"""
+            """ Test Classin which a memoize wrapping is done """
 
             def a_method(self):
                 return 42
@@ -82,6 +82,31 @@ class TestGetJson(unittest.TestCase):
         
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
-        
+
+class TestGithubOrgClient(unittest.TestCase):
+
+    def setUp(self):
+        # Set up fixtures
+        self.org_name = "my_org"
+        self.client = GithubOrgClient(self.org_name)
+        self.repo_list = ["repo1", "repo2", "repo3"]
+        self.repo_license_list = [{"name": "repo1", "license": {"name": "apache-2.0"}},
+                                  {"name": "repo2", "license": None},
+                                  {"name": "repo3", "license": {"name": "mit"}}]
+
+    def test_public_repos(self):
+        # Test public_repos method
+        repos = self.client.public_repos()
+        self.assertEqual(len(repos), len(self.repo_list))
+        for repo in repos:
+            self.assertIn(repo["name"], self.repo_list)
+
+    def test_public_repos_with_license(self):
+        # Test public_repos method with license argument
+        repos = self.client.public_repos(license="apache-2.0")
+        expected_repos = [repo for repo in self.repo_license_list if repo["license"] and repo["license"]["name"] == "apache-2.0"]
+        self.assertEqual(len(repos), len(expected_repos))
+        for repo in repos:
+            self.assertIn(repo["name"], [r["name"] for r in expected_repos])
 if __name__ == "__main__":
     unittest.main()
